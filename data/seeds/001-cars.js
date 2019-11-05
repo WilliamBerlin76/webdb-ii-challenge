9b6744a18f2411bb39bb84d52b5a22a99b00d8de
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-05 16:24:23 -0600

    added seed data to the database

--- data/seeds/001-cars.js
+++ data/seeds/001-cars.js
@@ -1,13 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('specs').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('specs').insert([
+        {id: 1, VIM: 835967, make: 'testing seeds', model: 'test model', mileage: 1254, type: 'new', status: 'nice'},
+        {id: 2, VIM: 14586, make: 'testing truncate', model: 'another one', mileage: 1}
       ]);
     });
 };
