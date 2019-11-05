530e59886626b125288b29b27e576e6a8891079f
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-05 17:45:41 -0600

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const carsRouter = require('./data/carsRoute');
 
@@ -10,6 +11,7 @@ server.get('/', (req, res) => {
 })
 
 server.use(helmet());
+server.use(cors());
 server.use(express.json());
 
 server.use('/api/cars', carsRouter);
